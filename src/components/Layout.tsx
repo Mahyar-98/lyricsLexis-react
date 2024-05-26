b1f44946da3f1560b5978946ed16b011c408544a
Mahyar <mahyar.erfanian1998@gmail.com>
2024-05-25 20:32:15 -0400

    add Layout styling

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -74,6 +74,10 @@ const Layout = () => {
     }
   };
 
+  const handleLinkClick = () => {
+    setNavOpen(false); // Close the side navigation
+  };
+
   const handleSignOut = () => {
     // Clear the authentication token from local storage
     localStorage.removeItem("token");
@@ -87,6 +91,12 @@ const Layout = () => {
     <div className="layout">
       <header>
         <div>
+          <div className="logo">
+            <Link to="/">
+              <h1>LyricsLexis</h1>
+            </Link>
+          </div>
+
           <button
             id="nav-toggle"
             aria-label="Toggle navigation"
@@ -119,7 +129,9 @@ const Layout = () => {
                     <Link to="/library">library</Link>
                   </li>
                   <li>
-                    <button onClick={handleSignOut}>sign out</button>
+                    <a href="#" onClick={handleSignOut}>
+                      sign out
+                    </a>
                   </li>
                 </>
               )}
@@ -132,7 +144,7 @@ const Layout = () => {
           <h2>LyricsLexis</h2>
           <ul>
             <li>
-              <Link to="/">
+              <Link to="/" onClick={handleLinkClick}>
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
                   <path d="M575.8 255.5c0 18-15 32.1-32 32.1h-32l.7 160.2c0 2.7-.2 5.4-.5 8.1V472c0 22.1-17.9 40-40 40H456c-1.1 0-2.2 0-3.3-.1c-1.4 .1-2.8 .1-4.2 .1H416 392c-22.1 0-40-17.9-40-40V448 384c0-17.7-14.3-32-32-32H256c-17.7 0-32 14.3-32 32v64 24c0 22.1-17.9 40-40 40H160 128.1c-1.5 0-3-.1-4.5-.2c-1.2 .1-2.4 .2-3.6 .2H104c-22.1 0-40-17.9-40-40V360c0-.9 0-1.9 .1-2.8V287.6H32c-18 0-32-14-32-32.1c0-9 3-17 10-24L266.4 8c7-7 15-8 22-8s15 2 21 7L564.8 231.5c8 7 12 15 11 24z" />
                 </svg>
@@ -140,9 +152,9 @@ const Layout = () => {
               </Link>
             </li>
             <li>
-              <Link to="about">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
-                  <path d="M64 32C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64H512c35.3 0 64-28.7 64-64V96c0-35.3-28.7-64-64-64H64zm80 256h64c44.2 0 80 35.8 80 80c0 8.8-7.2 16-16 16H80c-8.8 0-16-7.2-16-16c0-44.2 35.8-80 80-80zm-32-96a64 64 0 1 1 128 0 64 64 0 1 1 -128 0zm256-32H496c8.8 0 16 7.2 16 16s-7.2 16-16 16H368c-8.8 0-16-7.2-16-16s7.2-16 16-16zm0 64H496c8.8 0 16 7.2 16 16s-7.2 16-16 16H368c-8.8 0-16-7.2-16-16s7.2-16 16-16zm0 64H496c8.8 0 16 7.2 16 16s-7.2 16-16 16H368c-8.8 0-16-7.2-16-16s7.2-16 16-16z" />
+              <Link to="about" onClick={handleLinkClick}>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                  <path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM216 336h24V272H216c-13.3 0-24-10.7-24-24s10.7-24 24-24h48c13.3 0 24 10.7 24 24v88h8c13.3 0 24 10.7 24 24s-10.7 24-24 24H216c-13.3 0-24-10.7-24-24s10.7-24 24-24zm40-208a32 32 0 1 1 0 64 32 32 0 1 1 0-64z" />
                 </svg>
                 <p>About</p>
               </Link>
@@ -153,7 +165,7 @@ const Layout = () => {
             {!session ? (
               <>
                 <li>
-                  <Link to="contact">
+                  <Link to="signin" onClick={handleLinkClick}>
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
                       viewBox="0 0 512 512"
@@ -164,7 +176,7 @@ const Layout = () => {
                   </Link>
                 </li>
                 <li>
-                  <Link to="contact">
+                  <Link to="signup" onClick={handleLinkClick}>
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
                       viewBox="0 0 640 512"
@@ -178,7 +190,7 @@ const Layout = () => {
             ) : (
               <>
                 <li>
-                  <Link to="library">
+                  <Link to="library" onClick={handleLinkClick}>
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
                       viewBox="0 0 448 512"
@@ -189,7 +201,13 @@ const Layout = () => {
                   </Link>
                 </li>
                 <li>
-                  <button onClick={handleSignOut}>
+                  <a
+                    href="#"
+                    onClick={() => {
+                      handleSignOut();
+                      handleLinkClick();
+                    }}
+                  >
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
                       viewBox="0 0 512 512"
@@ -197,7 +215,7 @@ const Layout = () => {
                       <path d="M377.9 105.9L500.7 228.7c7.2 7.2 11.3 17.1 11.3 27.3s-4.1 20.1-11.3 27.3L377.9 406.1c-6.4 6.4-15 9.9-24 9.9c-18.7 0-33.9-15.2-33.9-33.9l0-62.1-128 0c-17.7 0-32-14.3-32-32l0-64c0-17.7 14.3-32 32-32l128 0 0-62.1c0-18.7 15.2-33.9 33.9-33.9c9 0 17.6 3.6 24 9.9zM160 96L96 96c-17.7 0-32 14.3-32 32l0 256c0 17.7 14.3 32 32 32l64 0c17.7 0 32 14.3 32 32s-14.3 32-32 32l-64 0c-53 0-96-43-96-96L0 128C0 75 43 32 96 32l64 0c17.7 0 32 14.3 32 32s-14.3 32-32 32z" />
                     </svg>
                     <p>sign out</p>
-                  </button>
+                  </a>
                 </li>
               </>
             )}
